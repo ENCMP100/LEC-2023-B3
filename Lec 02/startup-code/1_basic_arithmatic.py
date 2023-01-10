d44fcf3f581c86651804e5f3e1707e99fc2329ad
KamalRanaweera <ranaweer@ualberta.ca>
2023-01-09 23:56:40 -0700

    Added start-u code for lecture 2

---
+++ Lec 02/startup-code/1_basic_arithmatic.py
@@ -0,0 +1,48 @@
+"""
+Created on Sun Jan  8 16:57:19 2023
+
+Run the program and review values in the Variable Explorer in Spyder
+
+@author: ranaweer
+"""
+
+# define sime variables
+x = 5
+y = 2
+n = 3
+
+
+# Addition of x and y
+#add_result = 
+
+# Subtraction between x and y
+#sub_result = 
+
+# Floating point Division x and y
+#division_result = 
+
+# Integer division between x and y
+#integer_division_result = 
+
+# Integer division (floor division) between x and y
+#remainder_result = 
+
+# Negating a variable value x
+#negation_result = 
+
+# Exponent of x to the n
+#exponent_result = 
+
+
+"""
+ Operator precedence (order of performing operations)
+ BEDMAS
+ 
+ Ex. 
+                x + y
+    average_xy = -----
+                  2
+"""
+#average_xy = 
+
+
---
+++ Lec 02/startup-code/4_plot_basics.py
@@ -0,0 +1,26 @@
+"""
+Created on Sun Jan  8 20:06:01 2023
+
+Run the program and review graph in the Plots window in Spyder
+
+@author: ranaweer
+"""
+
+from numpy import arange
+from matplotlib import pyplot
+"""
+    Alternate method: 
+        import matplotlib.pyplot as pyplot
+"""
+
+
+x = arange(-10, 11)
+print("x =", x)
+
+y = x ** 2
+print(y)
+
+
+
+
+
