a8fd5d3206ce22ab96fde0c3b9f839eb6a804b76
KamalRanaweera <ranaweer@ualberta.ca>
2023-01-11 15:43:56 -0700

    Work-in progress on lect 2

---
+++ Lec 02/completed-code/3_math-module_functions.py
@@ -0,0 +1,51 @@
+"""
+Created on Sun Jan  8 18:56:01 2023
+
+Run the program and review values in the Variable Explorer in Spyder
+
+@author: ranaweer
+"""
+
+from math import *
+#import math as math
+
+# Square Root of a number
+x1 = 25
+square_root_value = sqrt(x1)
+
+# Use exponential operator and square root function to caluclate
+# hypotenuse of a right-angle triangle
+a = 4
+b = 3
+hypotenuse = sqrt(a**2 + b**2)
+
+# Truncate a floating point number to an integer
+x2 = 7.75
+truncated_value = trunc(x2)
+
+
+# Converting degrees to radians
+myDeg = 30
+radian_value = radians(myDeg)
+
+# Converting radians to degrees
+#myRad = 0.525
+#degree_value =
+
+# sin, cos, and tan values of 30 degrees
+#angleInDegrees = 30
+
+#sin_value = 
+#cos_value = 
+#tan_value = 
+
+
+# Exponent to the exponent (e.g. e to-the x)
+#n = 3
+#e_to_the_3 =
+
+# Logarithm
+#natural_log_of_20 = 
+
+#base_10_log_of_1000 = 
+
