fb1d8fda566ea8954ae3ad168cbcbe56378dc5b7
KamalRanaweera <ranaweer@ualberta.ca>
2023-01-12 14:31:49 -0700

    Lecture 3 start-up code

---
+++ Lec 03/startup-code/1_lists_and_tuples.py
@@ -0,0 +1,38 @@
+
+"""
+Created on Wed Jan 11 16:17:37 2023
+
+@author: ranaweer
+"""
+
+# List
+# ====
+#
+# Basic Python "List" can hold multiple values of any type of data. e.g.
+# To define a List with a set of initial values, we can enclose the values 
+# within square brackets:
+my_list = [10, 'Hello World!', 200, 12.5]
+
+# Create a list that contains integers from 0 to 5
+#my_list2 = 
+
+# Create a list that contains integers from -2 to 2
+#my_list3 = 
+
+# Modify the first element in the my_list3 to 100
+#my_list3[0] = 
+
+# Create an example list of a lists as a two-dimensional
+#my_list4 = 
+
+
+# Tuple
+# =====
+#
+# Tuples also hold multiple values like a list, but once a tuple is created, 
+# they cannot be modified. To create a tuple, specify its values using parantheses
+my_tuple = (10, 'Hello World!', 200, 12.5)
+my_tuple2 = (4,2)
+my_tuple3 = (4,)  # Remember the comma. Otherwise, this will be an integer.
+
+# Try to change the first value in my_tuple to 100
