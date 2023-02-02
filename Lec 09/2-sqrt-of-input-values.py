18bbedc5fd6994dbc0fcd2a3d0fc822d59b7b303
KamalRanaweera <ranaweer@ualberta.ca>
2023-02-02 00:05:07 -0700

    Update 2-sqrt-of-input-values.py

--- Lec 09/2-sqrt-of-input-values.py
+++ Lec 09/2-sqrt-of-input-values.py
@@ -29,3 +29,23 @@ while num_string != "":
     # Take the next number
     num_string = input('Enter a number: ')
 
+
+# Example 3
+# In this example, we set the condition of the while loop 
+# to be True, so it will run as an infinite loop.
+# Hwoever, when the user enters an empty input, we
+# terminate the while loop by executing a 
+# "break" statement.
+while True:
+    num_string = input('Enter a number: ')
+    
+    if num_string == "":
+        break  # Break the loop, which skips the remaining 
+               # steps in the loop and terminates the loop.
+        
+    num = float(num_string)
+    # Calculate and print the square root
+    print("Square root of", num, "is", np.sqrt(num))
+    
+
+print("Program ended!")
