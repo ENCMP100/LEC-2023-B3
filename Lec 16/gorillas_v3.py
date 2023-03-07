898e7383b71a29e5c983d4acc5adecdf3aa4076a
KamalRanaweera <ranaweer@ualberta.ca>
2023-03-07 14:18:39 -0700

    Update gorillas_v3.py

--- Lec 16/gorillas_v3.py
+++ Lec 16/gorillas_v3.py
@@ -11,6 +11,7 @@ BW = 12
 splatX = []
 splatY = []
 
+## Main function that implements the high-level program flow
 def main():
     (stageX, stageY) = createStage()
     
@@ -25,6 +26,7 @@ def main():
         showScene(stageX, stageY, playerIndicies, trajectoryX, trajectoryY)
 
         # Player 1 is playing
+        # ===================
         (angle, velocity) = getThrowParameters(0)
         (trajectoryX, trajectoryY, splatBuildingIndex) = calcTrajectory(
             stageX, stageY, 0, playerIndicies[0], angle, velocity)
@@ -35,7 +37,9 @@ def main():
             winner = 'Player 1'
             break # breaks the while loop
         
+            
         # Player 2 is playing
+        # ===================
         (angle, velocity) = getThrowParameters(1)
         (trajectoryX, trajectoryY, splatBuildingIndex) = calcTrajectory(
             stageX, stageY, 0, playerIndicies[1], angle, velocity)
@@ -45,33 +49,40 @@ def main():
         if splatBuildingIndex == playerIndicies[0]:
             winner = 'Player 2'
             break # breaks the while loop
+                   
             
         # Check if the user intends to play another round
-        play_again = input('Enter Y to play another round:')
-        if not (play_again == 'Y' or play_again == 'y'):
+        # ===============================================
+        exit_game = input('\nEnter X to exit the game:')
+        if exit_game == 'X' or exit_game == 'x':
             play = False
 
     
     # Done playing
+    # ============
     if not winner == '':
         print(winner, 'Won! Game Over ...')
     
     print('Thank you for playing.')
     
     
-    
+## CREATE STAGE: creates the x and y coordinates of the stage that represents
+#  the buildings  
 def createStage():   
     stageX = np.arange(0, N) * BW #Positions (m).
     stageY = (1 + np.random.random(stageX.shape) * N) * N/2
     return (stageX, stageY)
 
+## GET PLAYER INDICIES: returns randomly selected buildings indicies for players.
 def getPlayerIndicies(stageX, stageY):
     # First player on a building in the first half of the stage and the second
     # player on the second half of the stage. Skip the first and last buildings
     index1 = np.random.randint(1, N//2)
-    index2 = np.random.randint(N//2, N-302)
+    index2 = np.random.randint(N//2, N-2)
     return (index1, index2)  
     
+## SHOW SCENE: displays the buildings and places the players on top of them.
+#  Also, plots the trajectory of a throw if the coordinates were provided.
 def showScene(stageX, stageY, playerIndicies, trajectoryX, trajectoryY):
     
     N = len(stageX)
@@ -80,7 +91,7 @@ def showScene(stageX, stageY, playerIndicies, trajectoryX, trajectoryY):
     plt.bar(stageX, stageY, width=BW*0.9)
     plt.axis([0, max(stageX), 0, np.round(N**2)])
     plt.plot(stageX[playerIndicies[0]], stageY[playerIndicies[0]]+4, '*r', markersize=15)
-    plt.plot(stageX[playerIndicies[1]], stageY[playerIndicies[1]]+4, '*r', markersize=15)
+    plt.plot(stageX[playerIndicies[1]], stageY[playerIndicies[1]]+4, '*b', markersize=15)
    
     # Plotting the trajectory of the banana
     if len(trajectoryY) > 0:
@@ -91,7 +102,12 @@ def showScene(stageX, stageY, playerIndicies, trajectoryX, trajectoryY):
     
     plt.show()
  
+## GET THROW PARAMETERS: obtains the angle and velocity of throw for a given 
+#  player as user inputs and returns them. Angle is obtained in degrees as an
+#  acute andgle but returns its value in radians measured from the standard
+#  horizontal axix and measured in counter-clockwise 
 def getThrowParameters(playerNumber):
+    print()
     if playerNumber == 0: # giving first turn to the first player
         print("Player 1 (facing east)")
         angle = float(input("    Angle (degrees): "))
@@ -104,7 +120,9 @@ def getThrowParameters(playerNumber):
     
     return (angle, velocity)
 
-def calcTrajectory(stageX, stageY, playerNumber, playerIndex, a, v):
+## CALC TRAJECTOR: Calculates the trajectory of a throw for a given player
+#  for a given angle and velocity
+def calcTrajectory(stageX, stageY, playerNumber, playerBuildingIndex, a, v):
     
     global splatX
     global splatY
@@ -115,8 +133,8 @@ def calcTrajectory(stageX, stageY, playerNumber, playerIndex, a, v):
     x = v *  np.cos(a) * t
     y = v * np.sin(a) * t - 0.5 * g * t**2
     
-    x = stageX[playerIndex] + x
-    y = stageY[playerIndex] + y
+    x = stageX[playerBuildingIndex] + x
+    y = stageY[playerBuildingIndex] + 4 + y
     
     
     #Stop the banana when it splats on a building
@@ -137,7 +155,7 @@ def calcTrajectory(stageX, stageY, playerNumber, playerIndex, a, v):
             # Hits the building
             np.append(splatX, x[i])
             np.append(splatY, y[i])
-            splatBuildingIndex = i
+            splatBuildingIndex = buildingIndex
             break # breaks the for loop
             
     # we do not need any trajectory beyond the 
@@ -148,4 +166,5 @@ def calcTrajectory(stageX, stageY, playerNumber, playerIndex, a, v):
     return (x, y, splatBuildingIndex)
     
  
+## Calls the main function to start the program
 main()
