b16c5793a35d8b59492dcfa46fc79038ef0853cf
KamalRanaweera <ranaweer@ualberta.ca>
2023-03-09 00:43:15 -0700

    Lecture 17 scripts

---
+++ Lec 17/2-strings-manipulations.py
@@ -0,0 +1,72 @@
+## String Manipulation
+#  ===================
+
+
+# REPLACE: replaces a given substring with a another string
+sentense = "Thin slices of apples should be used to make apple pie"
+print(sentense)
+print(sentense.replace("apple", "banana"))
+
+## STRIP: strips white spaces or given specific characters from the begining
+#         and end of a string
+sentense = " Hello World \n"
+print(sentense)
+print("----") # Print a line after the previous print statements. See the blank line
+sentense2 = sentense.strip()
+print(sentense2)
+print("----") # No blank line above this line
+
+## RSTRIP: strips white spaces or given specific characters at the end of a string
+sentense = " Hello World \n"
+sentense3 = sentense.rstrip()
+print(sentense)
+print(sentense3) # White space at the begining is retained.
+
+# Using STRIP and RSTRIP to strip non-white spaces
+url = "https://www.ualberta.ca/"
+
+urlWithoutTrailingSlash = url.rstrip("/")
+print(urlWithoutTrailingSlash)
+
+domainName = urlWithoutTrailingSlash.strip("https://")
+print(domainName)
+
+
+# SPLIT: splits a string by white spaces or a given substring
+sentense = "Mary had a little       lamb"
+words = sentense.split() # split by white spaces. Removes empty strings
+                         # created by multiple consecutive white spaces
+                         # The result is returned as a list
+print(words)
+
+parts = sentense.split(maxsplit=1) # Splits only once, starting from the begining 
+print(parts)
+
+# RSPLIT: splits from the end. Useful only when we set maxsplit
+parts2 = sentense.rsplit(maxsplit=1) # Splits only once, starting from the end
+print(parts2)
+
+# SPLIT and RSPLIT with a given character
+csv = "12,4,54,455,213,34,6,78,0,16" # comma-separated value string
+vals = csv.split(",")
+print(vals)
+
+parts3 = csv.rsplit(",", maxsplit=1)
+print(parts3)
+
+## SPLITLINES: splits a string by new-line characters
+
+rhyme = "Row, row, row your boat\nGently down the stream\nMerrily, merrily, merrily, merrily\nLife is but a dream"
+print(rhyme)
+
+lines = rhyme.splitlines()
+print(lines)
+
+
+
+
+
+
+
+
+
